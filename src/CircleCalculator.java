083fd05d3584cbd8e284f6a90ec75c6521a1daed
jacall2016 <56280014+jacall2016@users.noreply.github.com>
2019-10-11 11:53:47 -0600

    added while loop to reprompt if radius is negitive.

--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -5,20 +5,24 @@ public class CircleCalculator {
     private static Scanner scanner = new Scanner( System.in );
 
     private double getRadius(){
-        System.out.println("Enter radius: ");
-        String input = scanner.nextLine();
-        double radius = Integer.parseInt(input);
+        double radius = 0;
+
+        while (radius < 0) {
+            System.out.println("Enter circle radius: ");
+            String input = scanner.nextLine();
+            radius = Integer.parseInt(input);
+        }
         return radius;
     }
 
     private void displayCircumference(double radius){
         double circ = Math.PI * radius * 2;
-        System.out.format("\tCircumference: %,.2f%n", circ);
+        System.out.format("\tcircle Circumference: %,.2f%n", circ);
     }
 
     public void displayArea(double radius){
         double area = Math.PI * radius * radius;
-        System.out.format("\tArea: %,.2f%n", area);
+        System.out.format("\tcircle Area: %,.2f%n", area);
     }
 
     public static void main(String[] args) {
